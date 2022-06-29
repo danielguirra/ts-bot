3d390f90e666cfad26a9889df1261e4776ebe03c
Daniel <daniel.guirra777@gmail.com>
2022-06-29 18:56:19 -0300

    pokedex

--- src/command/commandPokedex.ts
+++ src/command/commandPokedex.ts
@@ -1,7 +1,9 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { CommandInteraction, Message } from 'discord.js';
+import { getPokemon, Pokemon } from 'pkmonjs';
 
 import { embedBuilder } from '../../src/util/getEmbed';
+import { channelItsGuildTextChannel } from '../util/channelItsGuildTextChannel';
 
 /**
  * Don't forget to export
@@ -10,11 +12,70 @@ import { embedBuilder } from '../../src/util/getEmbed';
  * @danielguirra
  */
 export const pokedex = {
-  data: new SlashCommandBuilder().setName('pokedex').setDescription('pokedex'),
+  data: new SlashCommandBuilder()
+    .setName('pokedex')
+    .setDescription('pokedex')
+    .addStringOption(option =>
+      option
+        .setDescription('pokemon name')
+        .setName('pokemon')
+        .setRequired(true),
+    ),
   async executeMessageCommand(commandMessage: Message) {
-    return commandMessage.reply({ embeds: [embedBuilder('', '')] });
+    const poke = commandMessage.content.replace('*pokedex ', '');
+    try {
+      const pokedex = await getPokemon(poke);
+      const embed = embedBuilderPokedex(pokedex);
+
+      return commandMessage.reply({ embeds: [embed] });
+    } catch (error) {
+      return commandMessage.reply('verify pokemon name');
+    }
   },
   async executeSlashCommand(commandSlash: CommandInteraction) {
-    return commandSlash.reply({ embeds: [embedBuilder('', '')] });
+    const poke = commandSlash.options.getString('pokemon');
+
+    if (poke) {
+      const channel = await channelItsGuildTextChannel(commandSlash.channel);
+      const sender = commandSlash.reply('Pesquisando...').then(async () => {
+        const last = channel.lastMessage;
+        try {
+          const pokedex = await getPokemon(poke);
+          const embed = embedBuilderPokedex(pokedex);
+          last?.edit({ embeds: [embed] });
+        } catch (error) {
+          last?.edit('verify pokemon name');
+        }
+      });
+    }
   },
 };
+
+function embedBuilderPokedex(pokemon: Pokemon) {
+  return embedBuilder(
+    pokemon.name.toLocaleUpperCase(),
+    `${pokemon.description}
+   
+    *Status:*
+    Hit-points:${pokemon.stats.hp}❤
+    Attack:${pokemon.stats.attack}⚔
+    Defense:${pokemon.stats.defense}🛡
+    Special Attack:${pokemon.stats.specialAttack}☄
+    Special Defense:${pokemon.stats.specialDefense}🌟
+    Speed:${pokemon.stats.speed}💨
+    Height:${pokemon.stats.height}🏔
+    Weight:${pokemon.stats.weight}⚖
+
+    Habitat:${pokemon.habitat.name}
+
+    Generation:${pokemon.generation.name}
+
+    `,
+    pokemon.image.default,
+    pokemon.name,
+    pokemon.image.default,
+    pokemon.image.default,
+    'RED',
+    'https://www.pokemon.com/br/pokedex/' + pokemon.name,
+  );
+}
