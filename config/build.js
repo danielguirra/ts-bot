6f73a7d28df9d28680aa5b835c91ed8a292287af
Daniel <daniel.guirra777@gmail.com>
2022-08-01 02:33:53 -0300

    package scripts config

--- config/build.js
+++ config/build.js
@@ -1,11 +1,12 @@
 const { exec } = require('child_process');
 const { users } = require('../util/fileUsers/users');
 const fs = require('fs');
+const { prompt } = require('prompt');
 const { envs } = require('../util/fileEnv/env');
 const dir = './bin';
 const env = '/.env';
 envs();
-users();
+// users();
 
 if (!fs.existsSync(dir)) {
   //Efetua a criação do diretório
@@ -16,3 +17,8 @@ exec('npx tsc', f => {
   if (f) console.log(f.message);
   else console.log('Build Ok');
 });
+// console.log('Olá qual seu nome ?');
+// process.stdin.on('data', f => {
+//   console.log('Prazer ' + f);
+//   console.log(process.resourceUsage());
+// });
